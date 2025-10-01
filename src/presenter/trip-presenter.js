4494d1b8c5a4c172943fa0645f21d731e0532754
VITT.Ivanov <vitaly.ivanov@mail.ru>
2025-10-01 16:26:01 +0300

    задание:Шаблонизируй это

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -3,12 +3,20 @@ import SortingView from '../view/sorting-view.js';
 import EventListView from '../view/event-list-view.js';
 import EventItemView from '../view/event-item-view.js';
 import FormEditPointView from '../view/form-edit-point-view.js';
-import { render, RenderPosition } from '../render.js';
+import { render, RenderPosition } from '../utils/render.js';
+import TripModel from '../model/points-model.js';
+import points from '../mock/points.js';
+import destinations from '../mock/destination.js';
+import offersByType from '../mock/offer.js';
+
+const allTypes = offersByType.map((offer) => offer.type);
 
 export default class TripPresenter {
   constructor({ tripControlsContainer, tripEventsContainer }) {
     this._tripControlsContainer = tripControlsContainer;
     this._tripEventsContainer = tripEventsContainer;
+
+    this._tripModel = new TripModel({ points, destinations, offersByType });
   }
 
   init() {
@@ -32,13 +40,22 @@ export default class TripPresenter {
     render(eventsListComponent, this._tripEventsContainer, RenderPosition.BEFOREEND);
 
     const eventListContainer = eventsListComponent.getElement();
+    const tripPoints = this._tripModel.getPoints();
 
-    const formEditPointComponent = new FormEditPointView();
-    render(formEditPointComponent, eventListContainer, RenderPosition.AFTERBEGIN);
+    tripPoints.forEach((point) => {
+      const destination = this._tripModel.getDestinationsById(point.destination);
+      const offers = this._tripModel.getOffersForType(point.type);
 
-    for (let i = 0; i < 3; i++) {
-      const eventItemComponent = new EventItemView();
+      const formEditPointComponent = new FormEditPointView({
+        point,
+        destination,
+        offers,
+        allTypes
+      });
+      render(formEditPointComponent, eventListContainer, RenderPosition.AFTERBEGIN);
+
+      const eventItemComponent = new EventItemView(point, destination, offers);
       render(eventItemComponent, eventListContainer, RenderPosition.BEFOREEND);
-    }
+    });
   }
 }
--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -1,4 +1,4 @@
-import { createElement } from '../render.js';
+import { createElement } from '../utils/render';
 
 function createSortingTemplate() {
   return `
